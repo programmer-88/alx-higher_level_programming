d1305fa98c61bd4f2cfbfbcff5e11cdb13b6e07b
programmer-88 <ianmbayyz@gmail.com>
2023-10-04 00:30:46 +0300

    00...99

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-for number in range(100):
-    print("{:02d}, ".format(number), end="")
-
-else:
-    print("{:02d},".format(number))
+for index in range(0, 99):
+    print("{:02d}".format(index), end=', ')
+print("{:02d}".format(index + 1))

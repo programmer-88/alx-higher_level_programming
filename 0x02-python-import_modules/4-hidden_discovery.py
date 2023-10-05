92c334f96b9895ad976d018b87f29021cf82df7e
programmer-88 <ianmbayyz@gmail.com>
2023-10-05 21:49:58 +0300

    Who are you?

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,7 +2,8 @@
 
 if __name__ == "__main__":
     import hidden_4
+    
     func = dir(hidden_4)
     for f in func:
         if f[0:2] != "__":
-            print("({:s}".format(f))
+            print("{:s}".format(f))

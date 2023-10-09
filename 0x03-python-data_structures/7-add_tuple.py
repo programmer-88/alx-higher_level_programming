7db10dd21e2e9c101be10285deadb69f13623530
programmer-88 <ianmbayyz@gmail.com>
2023-10-10 01:16:14 +0300

    Tuples addition

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,10 @@
+def add_tuple(tuple_a=(), tuple_b=()):
+    a1 = tuple_a[0] if len(tuple_a) >= 1 else 0
+    a2 = tuple_a[1] if len(tuple_a) >= 2 else 0
+    b1 = tuple_b[0] if len(tuple_b) >= 1 else 0
+    b2 = tuple_b[1] if len(tuple_b) >= 2 else 0
+
+    sum1 = a1 + b1
+    sum2 = a2 + b2
+
+    return (sum1, sum2)

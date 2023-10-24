76f54a77009d98230eeb4e266f0f1a6a52f749b4
programmer-88 <ianmbayyz@gmail.com>
2023-10-25 00:00:47 +0300

    Print a square

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+
+""" This module defines a class square """
+
+
+class Square:
+    """ Square class with a private instance attribute"""
+
+    def __init__(self, size=0) -> None:
+        """
+        initialize attribute
+
+        Args:
+            size: size of square
+        """
+
+        self.size = size
+
+    @property
+    def size(self):
+        """ get attribute value to be used in class"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+
+        if value < 0:
+            raise ValueError("value must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """ area of square"""
+        return self.__size ** 2
+
+    def my_print(self):
+        """ print the square with character #"""
+        if self.__size == 0:
+            print()
+        else:
+            for i in range(self.__size):
+                print("#"*self.__size)

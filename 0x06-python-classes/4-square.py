f61da4de4f7ddbd064ebf475327eeccc1af412cf
programmer-88 <ianmbayyz@gmail.com>
2023-10-24 23:53:45 +0300

    Access and update private attribute

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -7,9 +7,9 @@ class Square:
     """ Square class with a private instance attribute"""
 
     def __init__(self, size=0) -> None:
-        """ 
+        """
         initialize attribute
-        
+
         Args:
             size: size of square
         """
@@ -20,7 +20,7 @@ class Square:
     def size(self):
         """ get attribute value to be used in class"""
         return self.__size
-        
+
     @size.setter
     def size(self, value):
         if type(value) is not int:

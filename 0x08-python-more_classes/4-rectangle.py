df222a94d86f5df94ba65a8d366fb9cfb9b81243
programmer-88 <ianmbayyz@gmail.com>
2023-10-30 21:14:47 +0300

    Eval is magic

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -63,7 +63,3 @@ class Rectangle:
     def __repr__(self):
         """returns a string representation of the rectangle for reproduction"""
         return "Rectangle({:d}, {:d})".format(self.__width, self.__height)
-
-    def __del__(self):
-        """ Method that deletes an instance of Rectangle """
-        print("Bye rectangle...")

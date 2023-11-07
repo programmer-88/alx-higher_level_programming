3ab3d338dcb4255fcde1082f10141d2335da1125
programmer-88 <ianmbayyz@gmail.com>
2023-11-07 12:44:00 +0300

    Read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -3,7 +3,11 @@
 
 
 def read_file(filename=""):
-    """ reads text file UTF8 encoding and prints to stdout """
+    """ reads text file UTF8 encoding and prints to stdout 
+        Args:
+             filename: string to read and print
+             Returns: nothing
+    """
 
     with open(filename, "r", encoding='utf-8') as file:
         for line in file:

dad32785d86f6bb72b1b2e9a7205ffb078fb663c
programmer-88 <ianmbayyz@gmail.com>
2023-11-14 00:59:33 +0300

    Update #0

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -166,3 +166,22 @@ class Rectangle(Base):
         Returns a string representation of rectangle
         """
         return f"[Rectangle] ({self.id}) {self.x}/{self.y} {self.width}/{self.height}"
+
+    def update(self, *args):
+        """
+        update Rectange
+        """
+        if len(args) >= 1:
+            self.id = args[0]
+        
+        if len(args) >= 2:
+            self.width = args[1]
+        
+        if len(args) >= 3:
+            self.height = args[2]
+        
+        if len(args) >= 4:
+            self.x = args[3]
+        
+        if len(args) >= 5:
+            self.y = args[4]

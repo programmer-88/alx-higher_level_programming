5863c19deb36bfb45c69e6934fd127d1436430f9
programmer-88 <ianmbayyz@gmail.com>
2023-12-14 13:40:30 +0300

    Read user

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,6 +1,8 @@
---this script crates the database hbtn_0d_2 and user user_0d_2
-   --user_0d_2 password is user_0d_2 pwd
-   --if user or database exists the script should not fail
+-- creates the database hbtn_0d_2 and the user user_0d_2
+   -- user_0d_2 should have only SELECT privilege in the database hbtn_0d_2
+   -- The user_0d_2 password should be set to user_0d_2_pwd
+   -- If the database hbtn_0d_2 already exists, your script should not fail
+   -- If the user user_0d_2 already exists, your script should not fail
 
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_d_pwd';

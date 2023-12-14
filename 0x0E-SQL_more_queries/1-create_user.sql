684b66d7537a14e6dfbb7e1129b1cde95843b409
programmer-88 <ianmbayyz@gmail.com>
2023-12-14 13:06:11 +0300

    Root user

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -3,5 +3,5 @@
     --user_0d_1 password should be set to user_0d_1_pwd
     --if the user_0d_1 exists, the script should not fail
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost';
 FLUSH PRIVILEGES;

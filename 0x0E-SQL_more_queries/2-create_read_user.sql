df48c2526753a85cdc532a53e4248af286d9629c
programmer-88 <ianmbayyz@gmail.com>
2023-12-14 13:44:36 +0300

    Read user

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -3,6 +3,6 @@
    -- if user or database exists the script should not fail
 
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_d_pwd';
-GRANT SELECT ON 'hbtn_0d_2'.* TO 'user_0d_d'@'localhost';
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON `hbtn_0d_2`.* TO 'user_0d_2'@'localhost';
 FLUSH PRIVILEGES;

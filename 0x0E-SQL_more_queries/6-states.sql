1cdf428ea24b8201e1aa9a49756047479586ffc3
programmer-88 <ianmbayyz@gmail.com>
2023-12-14 20:04:46 +0300

    States table

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -2,7 +2,10 @@
     -- id INT unique, primary key and auto generated
     -- name VARCHAR(255)
 
-CREATE IF NOT EXISTS hbtn_0d_usa(
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa
+
+USE hbtn_0d_2
+CREATE  TABLE IF NOT EXISTS hbtn_0d_usa(
     id SERIAL PRIMARY KEY,
     name VARCHAR(255)
 );

75eed557b574e7e4e3cb088e718a99c8bd05aeed
programmer-88 <ianmbayyz@gmail.com>
2023-12-14 13:18:09 +0300

    Root user

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -2,7 +2,7 @@
     --user_0d_1 should ahve all privileges on MySQL server
     --user_0d_1 password should be set to user_0d_1_pwd
     --if the user_0d_1 exists, the script should not fail
-    
+
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost';
 FLUSH PRIVILEGES;

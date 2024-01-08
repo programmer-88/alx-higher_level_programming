18ac135c52fbdfbef4e20646cf4f8eb2fb374878
programmer-88 <ianmbayyz@gmail.com>
2024-01-08 22:07:07 +0300

    3 languages

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,8 +1,5 @@
 #!/usr/bin/node
-const line1 = "C is fun";
-const line2 = "Python is cool";
-const line3 = "JavaScript is amazing";
 
-console.log(line1);
-console.log(line2);
-console.log(line3);
+console.log("C is fun");
+console.log("Python is cool");
+console.log("JavaScript is amazing");

78a5ef6f8a7ee5632d34cf19c60056b7cb51f3b0
programmer-88 <ianmbayyz@gmail.com>
2024-06-24 14:57:43 +0300

    Get all states fix

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -25,12 +25,12 @@ if __name__ == "__main__":
 
     cursor = db.cursor()
 
-    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+    cursor.execute("SELECT * FROM states ORDER BY states.id")
 
-    states = cursor.fetchall()
+    rows = cursor.fetchall()
 
-    for state in states:
-        print(state)
+    for row in rows:
+        print(row)
 
     
     cursor.close()
